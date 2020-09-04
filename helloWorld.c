f8c5c18243edbdc117ad4d60d606708d2cb2a52c
MattBigge <matthewbigge918@gmail.com>
2020-09-04 20:28:32 +0000

    added HelloWorld program with only my name

---
+++ helloWorld.c
@@ -0,0 +1,15 @@
+/**
+ *
+ * Author: Matthew Bigge
+ *
+ * Hello World Program for GitHub
+ *
+ */
+
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc,char **argv){
+    printf("Matthew Bigge\n");
+ }
